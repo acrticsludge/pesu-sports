ea4b0d38dfb9108d0a160f27ab9f673719a5df8e
Nirandev C M <nirandevcm@gmail.com>
2025-11-03 16:53:52 +0530

    Nirandevs initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function page() {
   return (
     <div>
-      <h1>NIRANDEV C M IN THE CHAT NIGGA </h1>
+      <h1>NIRANDEV C M IN THE CHAT</h1>
     </div>
   );
 }
